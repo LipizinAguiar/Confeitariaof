f0e5c99071d0a54552e96c2b857daf20eba7adf1
Felipe Aguiar <felipe.aguiarpires@hotmail.com>
2025-11-26 23:08:19 +0000

    labubu

--- cardapio.js
+++ cardapio.js
@@ -1,2 +1,25 @@
+
+
+
+function criarIteCardapio(titulo, descricao, foto){
+
+
 const divItemCardapio = document.createElement('div')
-divI
+divItemCardapio.className = 'item-Cardapio'
+
+const h3Titulo = document.createElement('h3')
+h3Titulo.textContent = titulo
+
+const pDescricao = document.createElement('p')
+pDescricao.textContent = descricao
+pDescricao.className = 'descricao'
+
+
+const img = document.createElement('img')
+img.src = foto
+img.className = 'img-item'
+
+const divC =document.getElementById('cardapio')
+
+divItemCardapio.appendChild()
+}
