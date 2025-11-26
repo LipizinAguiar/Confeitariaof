885ec8ff10116f614bf075c7c1ef21a47cd48659
Felipe Aguiar <felipe.aguiarpires@hotmail.com>
2025-11-26 23:14:31 +0000

    .

--- cardapio.js
+++ cardapio.js
@@ -21,5 +21,13 @@ img.className = 'img-item'
 
 const divC =document.getElementById('cardapio')
 
-divItemCardapio.appendChild()
+divItemCardapio.appendChild(h3Titulo)
+divItemCardapio.appendChild(pDescricao)
+divItemCardapio.appendChild(img)
+
+divC.appendChild(divItemCardapio)
+
 }
+
+criarIteCardapio('Bolo de Chocolate','Um classico irresistivel com camadas de chocolate.', 
+    'https://montarumnegocio.com/wp-content/uploads/2024/07/como-fazer-bolo-de-cenoura-para-vender.jpg')
