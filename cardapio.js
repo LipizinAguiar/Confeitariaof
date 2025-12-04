eed7c83512180f5091179d78801a39bba515c0f3
Felipe Aguiar <felipe.aguiarpires@hotmail.com>
2025-12-04 20:19:18 -0300

    gg

--- cardapio.js
+++ cardapio.js
@@ -30,44 +30,22 @@ divC.appendChild(divItemCardapio)
 }
 
 function consultarCardapio() {
-  fetch('https://confeitaria-api-1.onrender.com/cardapio')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Erro na requisição');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data); // Verifique no console a resposta completa
-      if (Array.isArray(data)) {
-        // Iterando sobre o array de bolos
-        data.forEach(item => {
-          // Criando os itens do cardápio
-          criarItemCardapio(item.titulo, item.descricao, item.foto);
-        });
-      } else {
-        throw new Error('Formato de dados inesperado.');
-      }
-    })
-    .catch(error => {
-      console.error('Erro:', error);
-      document.getElementById('resultado').innerHTML = `Erro: ${error.message}`;
-    });
+
+fetch('https://confeitaria-api-1.onrender.com/cardapio')
+.then(response => {
+if (!response.ok) {
+throw new Error('Erro na requisição');
 }
-
-// Função que cria um item no cardápio dinamicamente
-function criarItemCardapio(titulo, descricao, foto) {
-  const cardapioContainer = document.getElementById('cardapio');
-  const itemCardapio = document.createElement('div');
-  itemCardapio.classList.add('item-cardapio');
-
-  // Adiciona conteúdo ao item de cardápio
-  itemCardapio.innerHTML = `
-    <h3>${titulo}</h3>
-    <p class="descricao">${descricao}</p>
-    <img class="img-item" src="${foto}" alt="${titulo}" />
-  `;
-
-  // Adiciona o item ao container
-  cardapioContainer.appendChild(itemCardapio);
+return response.json();
+})
+.then(data => {
+console.log(data);
+criarItemCardapio(data.titulo,
+data.descricao,
+data.foto)
+})
+.catch(error => {
+console.error('Erro:', error);
+document.getElementById('resultado').innerHTML = `Erro: ${error.message}`;
+});
 }
